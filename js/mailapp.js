b03ae0c7e14468c5e777b69e2c3ecc2684eb4eff
Mikkel Madsen <m@madsn.net>
2014-09-16 20:50:49 +0200

    fetch mails via ajax

--- js/mailapp.js
+++ js/mailapp.js
@@ -1,51 +1,77 @@
 var mailApp = angular.module('mailApp',[]);
 
-mailApp.controller('InboxController', ['$scope', function($scope){
-    $scope.greeting = "HEeeelloo";
+mailApp.controller('InboxController', ['$scope', '$http', function($scope, $http){
+
+    $scope.activeEmail = null;
 
     $scope.accounts = {
         'account1': {
             'name': 'Mikkel',
-            'folders': {
-                'inbox': {
-                    'name': 'inbox',
-                    'emails': [
-                        {'sender': 'Test', 'subject': 'test subject', date: '2014-09-01'},
-                        {'sender': 'Test 2', 'subject': 'test subject 2', date: '2014-08-01'}
-                    ]
-                },
-                'archived': {
-                    'name': 'archived',
-                    'emails': []
-                }
-            }
+            'folders' : [
+                'inbox',
+                'archived',
+                'personal'
+            ]
         },
         'account12' : {
             'name' : 'Mikkel 2',
-            'folders' : {
-                'inbox': {
-                    'name': 'inbox',
-                    'emails': [
-                        {'sender': 'Test', 'subject': 'test subject', date: '2014-09-01'},
-                        {'sender': 'Test 2', 'subject': 'test subject 2', date: '2014-08-01'}
-                    ]
-                },
-                'archived': {
-                    'name': 'archived',
-                    'emails': []
-                },
-                'personal': {
-                    'name': 'personal',
-                    'emails': []
-                }
-            }
+            'folders' : [
+                'inbox',
+                'archived'
+            ]
         }
     };
+
+    $scope.activeAccount = $scope.accounts['account1'];
+
+    $scope.switchAccount = function(account) {
+        $scope.activeAccount = $scope.accounts(account);
+    };
+
+    $scope.currentPage = 0;
+    $scope.pageSize = 10;
+    $scope.numberOfPages=function(){
+        return Math.ceil($scope.activeAccount.emails.length/$scope.pageSize);
+    }
+
+    $scope.getEmails = function(accountId) {
+        $http.get('webservices/getEmails.html').
+            success(function (data, status, headers, config) {
+                $scope.accounts[accountId].emails = data.emails;
+            }).
+            error(function (data, status, headers, config) {
+                // log error
+            });
+    };
+
+    $scope.getEmailById = function(emailId) {
+        $http.get('webservices/getEmailById.html?mailId=' + emailId).
+            success(function (data, status, headers, config) {
+                $scope.activeEmail = data;
+            }).
+            error(function (data, status, headers, config) {
+                // log error
+            });
+    };
+
+
+    $scope.selectEmail = function(emailId) {
+        $scope.getEmailById(emailId);
+    };
+
+    $scope.getEmails('account1');
+
 }]);
+
+mailApp.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start;
+        return input.slice(start);
+    }
+});
 
 $(function(){
     $("table").resizableColumns({
         store: window.store
     });
 });
-
